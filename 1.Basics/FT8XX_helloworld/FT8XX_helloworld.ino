40c4df64bf2ec1a10ba455261646baec52cb18bf
Tomas <lightcalamar@gmail.com>
2017-04-06 17:52:44 +0200

    Update FT8XX_helloworld.ino

--- 1.Basics/FT8XX_helloworld/FT8XX_helloworld.ino
+++ 1.Basics/FT8XX_helloworld/FT8XX_helloworld.ino
@@ -1,4 +1,4 @@
-//#include <EEPROM.h> 
+#include <EEPROM.h> 
 #include <SPI.h>
 #include <GD3.h>
 
@@ -13,6 +13,7 @@ void loop()
   GD.ClearColorRGB(0x000020);
   GD.Clear();
   GD.cmd_text(0, 0, 24, 0, "Pantalla FT811");
+  GD.cmd_button(625, 40, 150, 50, 29, 0, "Boton");
   GD.cmd_romfont(67, 34);    // Usando RON FONT NUMBER 34
   GD.cmd_text(GD.w / 2, GD.h / 2, 67, OPT_CENTER, "Hello world");
   // GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
