f878fd0c89d6985b95591fbd1f007fdb158249f2
Tomas <lightcalamar@gmail.com>
2017-04-06 17:34:37 +0200

    Update FT8XX_helloworld.ino
    Add sample 800x600 Hello Word!

--- 1.Basics/FT8XX_helloworld/FT8XX_helloworld.ino
+++ 1.Basics/FT8XX_helloworld/FT8XX_helloworld.ino
@@ -12,8 +12,12 @@ void loop()
 {
   GD.ClearColorRGB(0x000020);
   GD.Clear();
+  GD.cmd_text(0, 0, 24, 0, "Pantalla FT811");
   GD.cmd_romfont(67, 34);    // Usando RON FONT NUMBER 34
   GD.cmd_text(GD.w / 2, GD.h / 2, 67, OPT_CENTER, "Hello world");
   // GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
+  GD.VertexTranslateX(16*460);
+  //GD.VertexTranslateY(16*160);
+  GD.cmd_text(245, 450, 24, 0, "800x600");
   GD.swap();
 }
